206c3587f77308c7578b3cc9e5a7d1c559671136
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2024-02-01 04:32:16 -0300

    feat: clienteController e produtoController exportados como classes

--- src/controllers/ClienteController.ts
+++ src/controllers/ClienteController.ts
@@ -7,70 +7,64 @@ import { Request, Response } from "express";
   res.status(200).send("Funcionou");
 } */
 
-async function createUser(req: Request, res: Response) {
-  try {
-    //const { nome, cpf, email, senha, telefone } = req.body;
-    const cliente = await prisma.cliente.create({
-      data: req.body,
-    });
-    res.status(201).json(cliente);
-  } catch (error) {
-    res.status(400).json({ error });
+class ClienteController {
+  async createUser(req: Request, res: Response) {
+    try {
+      //const { nome, cpf, email, senha, telefone } = req.body;
+      const cliente = await prisma.cliente.create({
+        data: req.body,
+      });
+      res.status(201).json(cliente);
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  }
+
+  async getAllUsers(req: Request, res: Response) {
+    try {
+      const clientes = await prisma.cliente.findMany();
+      res.status(200).json(clientes);
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  }
+
+  async getUser(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const cliente = await prisma.cliente.findUnique({
+        where: { id: Number(id) },
+      });
+      res.status(200).json(cliente);
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  }
+
+  async updateUser(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const cliente = await prisma.cliente.update({
+        where: { id: Number(id) },
+        data: req.body,
+      });
+      res.status(200).json(cliente);
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  }
+
+  async deleteUser(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const cliente = await prisma.cliente.delete({
+        where: { id: Number(id) },
+      });
+      res.status(200).json(cliente);
+    } catch (error) {
+      res.status(400).json({ error });
+    }
   }
 }
-
-async function getAllUsers(req: Request, res: Response) {
-  try {
-    const clientes = await prisma.cliente.findMany();
-    res.status(200).json(clientes);
-  } catch (error) {
-    res.status(400).json({ error });
-  }
-}
-
-async function getUser(req: Request, res: Response) {
-  try {
-    const { id } = req.params;
-    const cliente = await prisma.cliente.findUnique({
-      where: { id: Number(id) },
-    });
-    res.status(200).json(cliente);
-  } catch (error) {
-    res.status(400).json({ error });
-  }
-}
-
-async function updateUser(req: Request, res: Response) {
-  try {
-    const { id } = req.params;
-    const cliente = await prisma.cliente.update({
-      where: { id: Number(id) },
-      data: req.body,
-    });
-    res.status(200).json(cliente);
-  } catch (error) {
-    res.status(400).json({ error });
-  }
-}
-
-async function deleteUser(req: Request, res: Response) {
-  try {
-    const { id } = req.params;
-    const cliente = await prisma.cliente.delete({
-      where: { id: Number(id) },
-    });
-    res.status(200).json(cliente);
-  } catch (error) {
-    res.status(400).json({ error });
-  }
-}
-
-const ClienteController = {
-  createUser,
-  getAllUsers,
-  getUser,
-  updateUser,
-  deleteUser,
-};
 
 export default ClienteController;
--- src/controllers/ProdutoController.ts
+++ src/controllers/ProdutoController.ts
@@ -3,69 +3,63 @@ const prisma = new PrismaClient();
 
 import { Request, Response } from "express";
 
-async function createProduto(req: Request, res: Response) {
-  try {
-    const produto = await prisma.produto.create({
-      data: req.body,
-    });
-    res.status(201).json(produto);
-  } catch (error) {
-    res.status(400).json({ error });
+class ProdutoController {
+  async createProduto(req: Request, res: Response) {
+    try {
+      const produto = await prisma.produto.create({
+        data: req.body,
+      });
+      res.status(201).json(produto);
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  }
+
+  async getAllProdutos(req: Request, res: Response) {
+    try {
+      const produtos = await prisma.produto.findMany();
+      res.status(200).json(produtos);
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  }
+
+  async getProduto(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const produto = await prisma.produto.findUnique({
+        where: { id: Number(id) },
+      });
+      res.status(200).json(produto);
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  }
+
+  async updateProduto(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const produto = await prisma.produto.update({
+        where: { id: Number(id) },
+        data: req.body,
+      });
+      res.status(200).json(produto);
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  }
+
+  async deleteProduto(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const produto = await prisma.produto.delete({
+        where: { id: Number(id) },
+      });
+      res.status(200).json(produto);
+    } catch (error) {
+      res.status(400).json({ error });
+    }
   }
 }
-
-async function getAllProdutos(req: Request, res: Response) {
-  try {
-    const produtos = await prisma.produto.findMany();
-    res.status(200).json(produtos);
-  } catch (error) {
-    res.status(400).json({ error });
-  }
-}
-
-async function getProduto(req: Request, res: Response) {
-  try {
-    const { id } = req.params;
-    const produto = await prisma.produto.findUnique({
-      where: { id: Number(id) },
-    });
-    res.status(200).json(produto);
-  } catch (error) {
-    res.status(400).json({ error });
-  }
-}
-
-async function updateProduto(req: Request, res: Response) {
-  try {
-    const { id } = req.params;
-    const produto = await prisma.produto.update({
-      where: { id: Number(id) },
-      data: req.body,
-    });
-    res.status(200).json(produto);
-  } catch (error) {
-    res.status(400).json({ error });
-  }
-}
-
-async function deleteProduto(req: Request, res: Response) {
-  try {
-    const { id } = req.params;
-    const produto = await prisma.produto.delete({
-      where: { id: Number(id) },
-    });
-    res.status(200).json(produto);
-  } catch (error) {
-    res.status(400).json({ error });
-  }
-}
-
-const ProdutoController = {
-  createProduto,
-  getAllProdutos,
-  getProduto,
-  updateProduto,
-  deleteProduto,
-};
 
 export default ProdutoController;
